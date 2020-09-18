db8ad166e7dd30e52ab2b1993952cc4eceb3d260
jordansantosj <67442300+jordansantosj@users.noreply.github.com>
2020-09-18 19:10:32 -0300

    Projeto 1 do video de React Hooks do Diego Fernandes

--- src/Api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const Api = axios.create({
-    baseURL: 'https://randomuser.me/api?results=10'
-})
-
-export default Api
--- src/App.js
+++ src/App.js
@@ -1,52 +1,69 @@
-import React, { Component } from 'react';
-import api from './Api';
-import { Table } from 'reactstrap'
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
+export default function App() {
+    const [repositories, setRepositories] = useState([]);
 
-    state = {
-        usuario: [],
+    useEffect(async () => {
+        const response = await fetch('https://api.github.com/users/jordansantosj/repos')
+        const data = await response.json()
+
+        setRepositories(data);
+    }, [])
+
+    useEffect(() => {
+        const filtered = repositories.filter(repo => repo.favorite)
+        document.title = `Você tem ${filtered.length} favoritos`
+    },[repositories])
+
+    function handleFavorite(id) {
+        const newRepositories = repositories.map(repo => {
+            return repo.id == id ? { ...repo, favorite: !repo.favorite } : repo
+        });
+
+        setRepositories(newRepositories);
     }
-    async componentDidMount() {
-        const response = await api.get()
-        this.setState({ usuario: response.data.results })
-    }
-    render() {
 
-        const { usuario } = this.state;
-        return (
-            <div>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Nome</th>
-                            <th>Data de nascimento</th>
-                            <th>Genero</th>
-                            <th>Email</th>
-                            <th>Telefone</th>
-                            <th>Location</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {console.log(usuario)}
-                        {usuario.map(info => (
-                            <tr key={info.id.value}>
-                                <th scope="row">{}</th>
-                        <td>{info.name.first} { info.name.last}</td>
-                                <td>{info.dob.date}</td>
-                                <td>{info.gender}</td>
-                                <td>{info.email}</td>
-                                <td>{info.phone}</td>
-                                <td>{info.location.city}/{info.location.state}/{info.location.country}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
-
-            </div>
-        )
-    }
+    return (
+        <ul>
+            {repositories.map(repo => (
+                <li key={repo.id}>
+                    {repo.name}{repo.favorite && <span>(Favorito)</span>}
+                    <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+                </li>
+            ))}
+        </ul>
+    );
 }
 
-export default App;
+
+        //     <div>
+        //         <Table>
+        //             <thead>
+        //                 <tr>
+        //                     <th>#</th>
+        //                     <th>Nome</th>
+        //                     <th>Data de nascimento</th>
+        //                     <th>Genero</th>
+        //                     <th>Email</th>
+        //                     <th>Telefone</th>
+        //                     <th>Location</th>
+        //                 </tr>
+        //             </thead>
+        //             <tbody>
+        //                 {console.log(usuario)}
+        //                 {usuario.map(info => (
+        //                     <tr key={info.id.value}>
+        //                         <th scope="row">{}</th>
+        //                 <td>{info.name.first} { info.name.last}</td>
+        //                         <td>{info.dob.date}</td>
+        //                         <td>{info.gender}</td>
+        //                         <td>{info.email}</td>
+        //                         <td>{info.phone}</td>
+        //                         <td>{info.location.city}/{info.location.state}/{info.location.country}</td>
+        //                     </tr>
+        //                 ))}
+        //             </tbody>
+        //         </Table>
+
+        //     </div>
+
