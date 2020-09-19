99bb112ef64f3c7e52fb366b5dbf1ec9073f5bd7
jordansantosj <67442300+jordansantosj@users.noreply.github.com>
2020-09-18 21:36:48 -0300

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,69 +1,48 @@
 import React, { useState, useEffect } from 'react';
+import { Table } from 'reactstrap'
 
 export default function App() {
-    const [repositories, setRepositories] = useState([]);
+    const [users, setUsers] = useState([]);
 
     useEffect(async () => {
-        const response = await fetch('https://api.github.com/users/jordansantosj/repos')
+        const response = await fetch('https://randomuser.me/api?results=10')
         const data = await response.json()
 
-        setRepositories(data);
+        setUsers(data.results);
     }, [])
 
-    useEffect(() => {
-        const filtered = repositories.filter(repo => repo.favorite)
-        document.title = `Você tem ${filtered.length} favoritos`
-    },[repositories])
-
-    function handleFavorite(id) {
-        const newRepositories = repositories.map(repo => {
-            return repo.id == id ? { ...repo, favorite: !repo.favorite } : repo
-        });
-
-        setRepositories(newRepositories);
-    }
+    console.log(users)
 
     return (
-        <ul>
-            {repositories.map(repo => (
-                <li key={repo.id}>
-                    {repo.name}{repo.favorite && <span>(Favorito)</span>}
-                    <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
-                </li>
-            ))}
-        </ul>
+           <div>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nome</th>
+                            <th>Data de nascimento</th>
+                            <th>Genero</th>
+                            <th>Email</th>
+                            <th>Telefone</th>
+                            <th>Location</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(info => (
+                            <tr key={info.id.value}>
+                                <th scope="row">{}</th>
+                        <td>{info.name.first} { info.name.last}</td>
+                                <td>{info.dob.date}</td>
+                                <td>{info.gender}</td>
+                                <td>{info.email}</td>
+                                <td>{info.phone}</td>
+                                <td>{info.location.city}/{info.location.state}/{info.location.country}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+
+            </div>
     );
 }
-
-
-        //     <div>
-        //         <Table>
-        //             <thead>
-        //                 <tr>
-        //                     <th>#</th>
-        //                     <th>Nome</th>
-        //                     <th>Data de nascimento</th>
-        //                     <th>Genero</th>
-        //                     <th>Email</th>
-        //                     <th>Telefone</th>
-        //                     <th>Location</th>
-        //                 </tr>
-        //             </thead>
-        //             <tbody>
-        //                 {console.log(usuario)}
-        //                 {usuario.map(info => (
-        //                     <tr key={info.id.value}>
-        //                         <th scope="row">{}</th>
-        //                 <td>{info.name.first} { info.name.last}</td>
-        //                         <td>{info.dob.date}</td>
-        //                         <td>{info.gender}</td>
-        //                         <td>{info.email}</td>
-        //                         <td>{info.phone}</td>
-        //                         <td>{info.location.city}/{info.location.state}/{info.location.country}</td>
-        //                     </tr>
-        //                 ))}
-        //             </tbody>
-        //         </Table>
-
-        //     </div>
 
